436f0a6f4bf9861b2ac1b8e18e0a886e0b1297f3
Stuart Hallows <stuart.hallows@gmail.com>
2024-05-20 21:55:40 +0930

    Initial commit

---
+++ FullTextSearchQuickstart/Hotel.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json.Serialization;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Indexes.Models;
+
+/*
+ SimpleField: is always non-searchable (it's ignored for full text search queries), and is retrievable (it's not hidden)
+You might use a SimpleField for document IDs or fields used only in filters, facets, or scoring profiles.
+https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/search/Azure.Search.Documents/src/Indexes/SimpleFieldAttribute.cs
+
+SearchableField: must be a string, is always searchable, and is retrievable. Because this field type is searchable, it
+supports synonyms and the full complement of analyzer properties
+https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/search/Azure.Search.Documents/src/Indexes/SearchableFieldAttribute.cs
+
+You must explicitly enable filter, facet, and sort attributes.
+ */
+
+namespace AzureSearch.Quickstart
+{
+    public partial class Hotel
+    {
+        [SimpleField(IsKey = true, IsFilterable = true)]
+        public string HotelId { get; set; }
+
+        [SearchableField(IsSortable = true)]
+        public string HotelName { get; set; }
+
+        [SearchableField(AnalyzerName = LexicalAnalyzerName.Values.EnLucene)]
+        public string Description { get; set; }
+
+        [SearchableField(AnalyzerName = LexicalAnalyzerName.Values.FrLucene)]
+        [JsonPropertyName("Description_fr")]
+        public string DescriptionFr { get; set; }
+
+        [SearchableField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public string Category { get; set; }
+
+        [SearchableField(IsFilterable = true, IsFacetable = true)]
+        public string[] Tags { get; set; }
+
+        [SimpleField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public bool? ParkingIncluded { get; set; }
+
+        [SimpleField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public DateTimeOffset? LastRenovationDate { get; set; }
+
+        [SimpleField(IsFilterable = true, IsSortable = true, IsFacetable = true)]
+        public double? Rating { get; set; }
+
+        [SearchableField]
+        public Address Address { get; set; }
+    }
+}
